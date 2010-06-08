e970bdb5eec4b72b03396284a6ea0cffa6b37f12
krzysztof <krzysztofdoroz@gmail.com>
2010-06-08 21:40:38 +0200

    Acive Worker sends respones to a queue

--- ActiveWorker/src/activeworker/Main.java
+++ ActiveWorker/src/activeworker/Main.java
@@ -23,7 +23,9 @@ import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
 import javax.jms.Session;
 import javax.jms.MessageConsumer;
+import javax.jms.MessageProducer;
 import javax.jms.ObjectMessage;
+import result.Result;
 import task.Task;
 
 
@@ -34,7 +36,25 @@ public class Main {
 
 
     public static void main(String[] args) throws NamingException {
-        Context context = null;
+
+        getRequest();
+        try {
+            System.out.println("sending job to PBS...");
+            sentToPBS();
+        } catch (FileNotFoundException ex) {
+           System.out.println("File not foud exception");
+        } catch (IOException ex) {
+            System.out.println("IO exception");
+        } catch (InterruptedException ex) {
+            System.out.println("Interrupted exception");
+        }
+
+
+    }
+
+
+    static void getRequest() throws NamingException{
+         Context context = null;
         ConnectionFactory factory = null;
         Connection connection = null;
         String factoryName = "ConnectionFactory";
@@ -43,7 +63,7 @@ public class Main {
         Session session = null;
         MessageConsumer receiver = null;
 
-        
+
 
         destName = "ClientRequests";
 
@@ -57,7 +77,7 @@ public class Main {
 
 
         try {
-            
+
             // look up the ConnectionFactory
             factory = (ConnectionFactory) context.lookup(factoryName);
 
@@ -77,14 +97,14 @@ public class Main {
             // start the connection, to enable message receipt
             connection.start();
 
-           
+
                 Message message = receiver.receive();
                 if (message instanceof ObjectMessage) {
                     ObjectMessage task = (ObjectMessage) message;
 
                     System.out.print(task.getObject().getClass());
                     Task t = (Task)task.getObject();
-                    
+
                     BufferedWriter out;
                 try {
                     out = new BufferedWriter(new FileWriter(new File("data.in")));
@@ -108,7 +128,7 @@ public class Main {
                 } catch (IOException ex) {
                     System.out.println("Exception occured while writing to data.in");
                 }
-                    
+
 
                     //computeAndSend(t);
 
@@ -116,7 +136,7 @@ public class Main {
                 } else if (message != null) {
                     System.out.println("Received non object message");
                 }
-            
+
         } catch (JMSException exception) {
             exception.printStackTrace();
         } catch (NamingException exception) {
@@ -140,6 +160,7 @@ public class Main {
                 }
             }
         }
+
         try {
             //wyslij zadanie do PBS-a
             sentToPBS();
@@ -151,13 +172,9 @@ public class Main {
             System.out.println("Interrupted exception");
         }
 
-
-
     }
 
-
-
-    static void sentToPBS() throws FileNotFoundException, IOException, InterruptedException{
+    static void sentToPBS() throws FileNotFoundException, IOException, InterruptedException, NamingException{
 
           String cmd = "qsub -W stagein=MonteCarlo.class@localhost:/home/students/krdoroz/lab3/MonteCarlo.class,data.in@localhost:/home/students/krdoroz/lab3/ActiveWorker/data.in hello.sh"; // this is the command to execute in the Unix shell
         // create a process for the shell
@@ -219,13 +236,99 @@ public class Main {
         BufferedReader reader = new BufferedReader(new FileReader(result));
 
         String line = "";
-        while ( (line = reader.readLine()) != null)
-            System.out.println(line);
+         line = reader.readLine();
+         line = reader.readLine();
+         String data[] = line.split(" ");
 
+         reader.close();
+
+         System.out.println(data[1]);
+         Double res = Double.parseDouble(data[1]);
+           
+
+        Result integrationResult = new Result();
+        integrationResult.setResult(res);
+
+        sentToResultQueue(integrationResult);
 
     }
     
-    
+    static void sentToResultQueue(Result result) throws NamingException{
+          Context context = null;
+        ConnectionFactory factory = null;
+        Connection connection = null;
+        String factoryName = "ConnectionFactory";
+        String destName = null;
+        Destination dest = null;
+
+        Session session = null;
+        MessageProducer sender = null;
+        String text = "Message ";
+
+         Properties props = new Properties();
+    props.put( Context.INITIAL_CONTEXT_FACTORY, "org.exolab.jms.jndi.InitialContextFactory" );
+    props.put( Context.PROVIDER_URL, "rmi://localhost:1299" );
+
+    //create initial context
+    context = new InitialContext( props );
+
+
+        destName = "Results";
+
+        try {
+
+            // look up the ConnectionFactory
+            factory = (ConnectionFactory) context.lookup(factoryName);
+
+            // look up the Destination
+            dest = (Destination) context.lookup(destName);
+
+            // create the connection
+            connection = factory.createConnection();
+
+            // create the session
+            session = connection.createSession(
+                false, Session.AUTO_ACKNOWLEDGE);
+
+            // create the sender
+            sender = session.createProducer(dest);
+
+            // start the connection, to enable message sends
+            connection.start();
+
+           
+                ObjectMessage message = session.createObjectMessage();
+
+                
+
+                message.setObject(result);
+                sender.send(message);
+                System.out.println("Sent result to a queue.. ");
+            
+        } catch (JMSException exception) {
+            exception.printStackTrace();
+        } catch (NamingException exception) {
+            exception.printStackTrace();
+        } finally {
+            // close the context
+            if (context != null) {
+                try {
+                    context.close();
+                } catch (NamingException exception) {
+                    exception.printStackTrace();
+                }
+            }
+
+            // close the connection
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (JMSException exception) {
+                    exception.printStackTrace();
+                }
+            }
+        }
+    }
 
     static double calculateFx(double a, double b, double c, double x){
 
