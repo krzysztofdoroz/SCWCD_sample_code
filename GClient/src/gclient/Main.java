5c76c22b6d47b12beeb32f7a1ad1fa432e5236cc
krzysztof <krzysztofdoroz@gmail.com>
2010-06-08 21:41:34 +0200

    Client can read results from a queuw

--- GClient/src/gclient/Main.java
+++ GClient/src/gclient/Main.java
@@ -9,9 +9,12 @@ import javax.jms.JMSException;
 import javax.jms.Destination;
 import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
 import javax.jms.MessageProducer;
 import javax.jms.ObjectMessage;
 import javax.jms.Session;
+import result.Result;
 import task.Task;
 
 
@@ -25,15 +28,105 @@ public class Main {
 
         sendRequests();
 
+        getResuls();
 
 
     }
 
-    static void getResuls(){
+    static void getResuls() throws NamingException{
 
-        for(int i = 0; i < COUNT; i++){
+         Context context = null;
+        ConnectionFactory factory = null;
+        Connection connection = null;
+        String factoryName = "ConnectionFactory";
+        String destName = null;
+        Destination dest = null;
+        Session session = null;
+        MessageConsumer receiver = null;
+        double totalResult = 0.0;
+
+
+        destName = "Results";
+
+
+         Properties props = new Properties();
+    props.put( Context.INITIAL_CONTEXT_FACTORY, "org.exolab.jms.jndi.InitialContextFactory" );
+    props.put( Context.PROVIDER_URL, "rmi://localhost:1299" );
+
+    //create initial context
+    context = new InitialContext( props );
+
+
+        try {
+
+            // look up the ConnectionFactory
+            factory = (ConnectionFactory) context.lookup(factoryName);
+
+            // look up the Destination
+            dest = (Destination) context.lookup(destName);
+
+            // create the connection
+            connection = factory.createConnection();
+
+            // create the session
+            session = connection.createSession(
+                false, Session.AUTO_ACKNOWLEDGE);
+
+            // create the receiver
+            receiver = session.createConsumer(dest);
+
+            // start the connection, to enable message receipt
+            connection.start();
+
+
             
+        for(int i = 0; i < COUNT; i++){
+
+        
+                Message message = receiver.receive();
+                if (message instanceof ObjectMessage) {
+                    ObjectMessage result = (ObjectMessage) message;
+
+                    System.out.print(result.getObject().getClass());
+                    Result t = (Result)result.getObject();
+
+                    totalResult += t.getResult();
+
+
+                    //computeAndSend(t);
+
+                    System.out.println("Received result message" );
+                } else if (message != null) {
+                    System.out.println("Received non object message");
+                }
         }
+        } catch (JMSException exception) {
+            exception.printStackTrace();
+        } catch (NamingException exception) {
+            exception.printStackTrace();
+        } finally {
+            // close the context
+            if (context != null) {
+                try {
+                    context.close();
+                } catch (NamingException exception) {
+                    exception.printStackTrace();
+                }
+            }
+
+            // close the connection
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (JMSException exception) {
+                    exception.printStackTrace();
+                }
+            }
+        }
+
+        System.out.println("TOTAL RESULT: " + totalResult);
+
+
 
 
     }
